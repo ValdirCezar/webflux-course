737f221bcf50c737fcf7ea40c0f5fe715b9e3abc
valdir_jesus <valdir_jesus@sicredi.com.br>
2023-01-04 15:13:14 -0300

    start project

---
+++ src/main/java/br/com/valdircezar/webfluxcourse/WebfluxCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.webfluxcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/valdircezar/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
