134a42edd5fd13eb116cc9186318d2e997571b28
valdir_jesus <valdir_jesus@sicredi.com.br>
2023-01-05 13:35:31 -0300

    add unit test to findAll user service

--- src/test/java/br/com/valdircezar/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/valdircezar/webfluxcourse/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -50,9 +51,7 @@ class UserServiceTest {
 
     @Test
     void testFindById() {
-        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder()
-                        .id("1234")
-                .build()));
+        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
 
         Mono<User> result = service.findById("123");
 
@@ -62,6 +61,20 @@ class UserServiceTest {
                 .verify();
 
         Mockito.verify(repository, times(1)).findById(anyString());
+    }
+
+    @Test
+    void testFindAll() {
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findAll();
     }
 
 }
