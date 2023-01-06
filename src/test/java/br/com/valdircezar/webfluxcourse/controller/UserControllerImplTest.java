14f41b655ef41062055b875e67264e9bd264e5a0
valdir_jesus <valdir_jesus@sicredi.com.br>
2023-01-06 15:09:58 -0300

    add integration test to delete endpoint with success

--- src/test/java/br/com/valdircezar/webfluxcourse/controller/UserControllerImplTest.java
+++ src/test/java/br/com/valdircezar/webfluxcourse/controller/UserControllerImplTest.java
@@ -131,7 +131,7 @@ class UserControllerImplTest {
 
     @Test
     @DisplayName("Test update endpoint with success")
-    void update() {
+    void testUpdateWithSuccess() {
         final var request = new UserRequest(NAME, EMAIL, PASSWORD);
         final var userResponse = new UserResponse(ID, NAME, EMAIL, PASSWORD);
 
@@ -156,6 +156,14 @@ class UserControllerImplTest {
     }
 
     @Test
-    void delete() {
+    @DisplayName("Test delete endpoint with success")
+    void testDeleteWithSuccess() {
+        when(service.delete(anyString())).thenReturn(just(User.builder().build()));
+
+        webTestClient.delete().uri("/users/" + ID)
+                .exchange()
+                .expectStatus().isOk();
+
+        verify(service).delete(anyString());
     }
 }
